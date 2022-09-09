08abbde3a6682101dd037163a6dc325a8a09d588
Felipe Espindola Valerio <felipe.valerio@dtidigital.com.br>
2022-09-09 00:19:13 -0300

    Implementando roteamento

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
-import { useState } from 'react';
 import { ThemeProvider } from 'styled-components';
-import { Button } from './components/Button';
 import { defaultTheme } from './styles/themes/default';
 import { GlobalStyle } from './styles/global';
+import { Router } from './Router';
+import { BrowserRouter }from 'react-router-dom';
 
 function App() {
-    const [count, setCount] = useState(0);
 
     return (
         <ThemeProvider theme={defaultTheme}>
-            <Button variants='primary'/>
-            <Button variants='secondary'/>
-            <Button variants='danger'/>
-            <Button variants='sucess'/>
+            <BrowserRouter>
+                <Router />
+            </BrowserRouter>
             <GlobalStyle/>
         </ThemeProvider>
     );
